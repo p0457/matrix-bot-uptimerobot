199dbc3cd7b6d0cbc1747e93001c21d546c165ab
Thomas Gardner <p0457@outlook.com>
2021-02-26 21:06:04 -0600

    Support GET as well as POST

--- src/index.ts
+++ src/index.ts
@@ -17,15 +17,8 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 const port = config.port;
 
-app.get("/webhook/:id", async (request, response) => {
-    LogService.error("app.get", "Method not allowed");
-    return response.status(405);
-});
-app.put("/webhook/:id", async (request, response) => {
-    LogService.error("app.put", "Method not allowed");
-    return response.status(405);
-});
-app.post("/webhook/:id", async (request, response) => {
+// Separate function is to support POST value as well as non (which is a GET, apparently)
+async function processWebhook(request: any, response: any): Promise<any> {
     const id = request.params.id;
     if (!id) {
         LogService.error("app.post", `id not provided`);
@@ -41,25 +34,37 @@ app.post("/webhook/:id", async (request, response) => {
         LogService.error("app.post", `roomId not found for id ${id}`);
         return response.status(204).send();
     }
-    if (!request.query.monitorID) {
+    const query = request.query || {};
+    if (!query.monitorID) {
         LogService.warn("app.post", `Payload was invalid, ignoring: ${JSON.stringify({ request })}`);
         return response.status(406).send();
     }
+    const body = request.body || {};
     const payload = {
-        monitorID: request.query.monitorID,
-        monitorURL: request.query.monitorURL,
-        monitorFriendlyName: request.query.monitorFriendlyName || "Unknown",
-        alertType: request.query.alertType,
-        alertTypeFriendlyName: request.query.alertTypeFriendlyName || "Unknown",
-        alertDetails: request.query.alertDetails || "Unknown",
-        alertDuration: request.query.alertDuration,
-        monitorAlertContacts: request.query.monitorAlertContacts,
-        sslExpiryDate: request.query.sslExpiryDate,
-        sslExpiryDaysLeft: request.query.sslExpiryDaysLeft
+        monitorID: query.monitorID || body.monitorID,
+        monitorURL: query.monitorURL || body.monitorURL,
+        monitorFriendlyName: query.monitorFriendlyName || body.monitorFriendlyName || "Unknown",
+        alertType: query.alertType || body.alertType,
+        alertTypeFriendlyName: query.alertTypeFriendlyName || body.alertTypeFriendlyName || "Unknown",
+        alertDetails: query.alertDetails || body.alertDetails || "Unknown",
+        alertDuration: query.alertDuration || body.alertDuration,
+        monitorAlertContacts: query.monitorAlertContacts || body.monitorAlertContacts,
+        sslExpiryDate: query.sslExpiryDate || body.sslExpiryDate,
+        sslExpiryDaysLeft: query.sslExpiryDaysLeft || body.sslExpiryDaysLeft
     };
     return webhookProcessor.processWebhook(id, roomId, payload).then(() => { return response.status(200).send(); }).catch(() => { return response.status(500).send(); });
-});
+}
 
+app.put("/webhook/:id", async (request, response) => {
+    LogService.error("app.put", "Method not allowed");
+    return response.status(405);
+});
+app.get("/webhook/:id", async (request, response) => {
+    return processWebhook(request, response);
+});
+app.post("/webhook/:id", async (request, response) => {
+    return processWebhook(request, response);
+});
 app.listen(port, () => {
     LogService.info("app.listen", `UptimeRobotBot Server running on port ${port}`);
 });
